123e551e56787d009808758e1ad2d2a221210630
Bri1515 <brisavale482@gmail.com>
2026-06-07 12:39:03 -0400

    fix: ajuste front para coneccion al backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -7,8 +8,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
+        target: 'https://localhost:3443',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
